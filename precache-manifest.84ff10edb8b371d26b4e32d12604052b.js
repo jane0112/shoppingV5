4687ba3daa8d7d988603fcff19ea0fe210bddd56
jane0112 <ninja9239@gmail.com>
2020-05-27 03:40:55 +0800

    Updates

---
+++ precache-manifest.84ff10edb8b371d26b4e32d12604052b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37ba5a6296bd47b113f8f2394f1f2610",
+    "url": "/shoppingV5/index.html"
+  },
+  {
+    "revision": "5dd1569242971d63bf34",
+    "url": "/shoppingV5/static/css/main.e6d5ccea.chunk.css"
+  },
+  {
+    "revision": "3ab9ca00f2a0cf9c021f",
+    "url": "/shoppingV5/static/js/2.6a6218cb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shoppingV5/static/js/2.6a6218cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dd1569242971d63bf34",
+    "url": "/shoppingV5/static/js/main.da84fed4.chunk.js"
+  },
+  {
+    "revision": "ffbd82d7bd123d839d20",
+    "url": "/shoppingV5/static/js/runtime-main.f4f93afa.js"
+  }
+]);
